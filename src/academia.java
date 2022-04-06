736d06b3243ca161cf3f973bedc0098027bed8c9
Aldo Souto <102553770+aldosouto@users.noreply.github.com>
2022-04-06 18:52:55 -0300

    Update academia.java

--- src/academia.java
+++ src/academia.java
@@ -12,6 +12,27 @@ public class academia {
             String Semanal;
             String Mensal;
             String Anual;
+            
+            
+           import java.util.Scanner; 
+            public class Cadastro {
+    
+    	 public static void main(String[] args) {
+             
+             String Diaria;
+             String Semanal;
+             String Mensal;
+             String Anual;
+             
+          // Neste modo pessoal já fica um a baixo (nome, CPF e Fone) do outro no cadastro
+             
+             Scanner scanner = new Scanner(System.in);
+             System.out.println("Digite seu nome: ");
+             String nome = scanner.next();
+             System.out.println("Digite seu CPF: "); 
+             int cpf = scanner.nextInt();
+             System.out.println("Digite o seu telefone com o DD: ");
+             int Fone = scanner.nextInt();
         }
 
     }}
